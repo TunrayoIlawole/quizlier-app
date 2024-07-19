6725a5a44e0a17e531e8dc7a13e86d274bd49771
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-07-19 16:13:46 +0100

    add home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
+  login(): void {
+    this.router.navigate(['/auth/login']);
+  }
+
+  signup() {
+    this.router.navigate(['/auth/register']);
+  }
 
 }
