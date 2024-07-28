2b88190dec16365b925da7123987d8129cf3b07d
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-07-28 18:46:06 +0100

    update reducers

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,12 +1,22 @@
 import { ActionReducerMap } from "@ngrx/store";
 import * as fromAuth from '../auth/state/auth.reducer'
+import * as fromOptions from '../admin/option/state/options.reducer';
+import * as fromQuestions from '../admin/question/state/question.reducer';
+import * as fromCategories from '../admin/category/state/category.reducer';
 
 export interface State {
     user: fromAuth.UserState;
+    option: fromOptions.OptionState;
+    question: fromQuestions.QuestionState;
+    category: fromCategories.CategoryState;
 
 }
 
 export const appReducers: ActionReducerMap<State> = {
-    user: fromAuth.AuthReducer
+    user: fromAuth.AuthReducer,
+    option: fromOptions.OptionReducer,
+    question: fromQuestions.QuestionReducer,
+    category: fromCategories.CategoryReducer
+
     
 }
